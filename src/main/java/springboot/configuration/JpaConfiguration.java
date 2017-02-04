79e60b1deee9798257e69d8347531b4b8458b420
2226327533 <2226327533@qq.com>
2017-02-04 17:04:58 +0800

    新添加mysql与springboot的集成

---
+++ src/main/java/springboot/configuration/JpaConfiguration.java
@@ -0,0 +1,28 @@
+package springboot.configuration;
+
+import org.springframework.boot.orm.jpa.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+
+
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * Created by xy on 2017/1/25.
+ */
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@Configuration
+@EnableTransactionManagement(proxyTargetClass = true)
+@EnableJpaRepositories(basePackages = "springboot.dao")
+@EntityScan(basePackages = "springboot.model")
+public class JpaConfiguration {
+    @Bean
+    PersistenceExceptionTranslationPostProcessor persistenceExceptionTranslationPostProcessor(){
+        return new PersistenceExceptionTranslationPostProcessor();
+    }
+}
